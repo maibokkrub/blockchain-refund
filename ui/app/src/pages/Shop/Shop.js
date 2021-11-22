b371981fd8842a4b94b4a37ed33404866bd7fa2b
ttrustt <thetrust31168@gmail.com>
2021-11-22 15:28:10 +0700

    Initialize UI of Shop page

--- ui/app/src/pages/Shop/Shop.js
+++ ui/app/src/pages/Shop/Shop.js
@@ -1,12 +1,49 @@
-import React from 'react';
-import { Container } from "@chakra-ui/react";
+import React, { useState } from 'react';
+import { Container, Flex, Spacer, Box, Stack, Input, Button, Center, Text } from "@chakra-ui/react";
 
 function ShopPage() {
-  return (
-    <Container minWidth={{base:'100vw', lg:'80vw'}} h='calc(100vh - 64px - 3rem)' bg='salmon'>
-        Shop Page
-    </Container> 
-  );
+    const [country, setCountry] = useState('');
+    const [shopName, setShopName] = useState('');
+    const [buyerAddress, setBuyerAddress] = useState('');
+    const [productDetail, setProductDetail] = useState('');
+    const [price, setPrice] = useState('');
+
+    return (
+        <Container maxW="1300px" h='calc(100vh - 64px - 3rem)'>
+            <Flex>
+                <Spacer />
+                <Box w="480px" p="4" bg="white" paddingBottom="45px" borderRadius="10px">
+                    <Center>
+                        <Text fontSize="28px" as="b" paddingTop="27px" paddingBottom="18px" color="#1a202c">Create Shop</Text>
+                    </Center>
+                    <Stack spacing={3}>
+                        <Input placeholder="Country" _placeholder={{ color: "#1a202c" }} size="md" variant="filled" bg="#90cdf4" color="#1a202c" value={country} />
+                        <Input placeholder="Shop Name" _placeholder={{ color: "#1a202c" }} size="md" variant="filled" bg="#90cdf4" color="#1a202c" value={shopName} />
+                    </Stack>
+                    <br />
+                    <Center>
+                        <Button bg="#1a202c">Create Shop</Button>
+                    </Center>
+                </Box>
+                <Spacer />
+                <Box w="480px" p="4" bg="white" paddingBottom="45px" borderRadius="10px">
+                    <Center>
+                        <Text fontSize="28px" as="b" paddingTop="27px" paddingBottom="18px" color="#1a202c">Create Order</Text>
+                    </Center>
+                    <Stack spacing={3}>
+                        <Input placeholder="Buyer Address" _placeholder={{ color: "#1a202c" }} size="md" variant="filled" bg="#90cdf4" color="#1a202c" value={buyerAddress} />
+                        <Input placeholder="Product Detail" _placeholder={{ color: "#1a202c" }} size="md" variant="filled" bg="#90cdf4" color="#1a202c" value={productDetail} />
+                        <Input placeholder="Price" _placeholder={{ color: "#1a202c" }} size="md" variant="filled" bg="#90cdf4" color="#1a202c" value={price} />
+                    </Stack>
+                    <br />
+                    <Center>
+                        <Button bg="#1a202c">Create Order</Button>
+                    </Center>
+                </Box>
+                <Spacer />
+            </Flex>
+        </Container>
+    );
 }
 
 export default ShopPage;
