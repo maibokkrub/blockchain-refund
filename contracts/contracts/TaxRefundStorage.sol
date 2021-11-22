9220a5aa15329c16c69b8435e773c43e23b6229f
Sorathan Kritveeranant <sorathan.krit@gmail.com>
2021-11-22 21:05:14 +0700

    Everything but refund

--- contracts/contracts/TaxRefundStorage.sol
+++ contracts/contracts/TaxRefundStorage.sol
@@ -1,22 +1,124 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract TaxRefundStorage {
-    string private greeting;
 
-    constructor(string memory _greeting) {
-        console.log("Deploying a Greeter with greeting:", _greeting);
-        greeting = _greeting;
+enum State { PENDING, CANCELED, CONFIRMED, REJECTED, REFUNDED }
+
+struct Shop {
+    string shopName;
+    string country;
+    bool exists;
+}
+
+struct Admin{
+    bool isAdmin;
+    string country;
+}
+
+struct Order {
+    bytes16 id;
+    string name;
+    int price;
+    int amount;
+    State state;
+    address buyer;
+    Shop shop;
+}
+
+contract TaxRefundStorage is Ownable {
+    using SafeMath for uint256;
+    mapping (bytes16 => Order) orders;
+    mapping (address => Shop) public shops;
+    // mapping (address => byte16[]) private _ordersByShop;
+    mapping (address => bytes16[]) private _ordersByBuyer;
+    mapping (address => Admin) private _admins;
+
+    // DECIMAL: 3 -> 00001000 = 1%
+    mapping (string => uint256) public vatPercentageByCountry;
+
+    constructor() {}
+
+    modifier onlyShop {
+        require(isShop(msg.sender), "Order Creator is not Shop.");
+        _;
+    }
+    
+    modifier onlyAdmin {
+        require(isAdmin(msg.sender), "Permission Denied");
+        _;
     }
 
-    function greet() public view returns (string memory) {
-        return greeting;
+    modifier buyerHasOrder(address buyer, bytes16 id) {
+        require(orders[id].buyer == buyer, "Invalid Buyer / Invalid Order's id");
+        _; 
     }
 
-    function setGreeting(string memory _greeting) public {
-        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-        greeting = _greeting;
+    function isAdmin(address addr) public view returns(bool){
+        return _admins[addr].isAdmin;
     }
+
+    function isShop(address addr) public view returns(bool) {
+        return shops[addr].exists;
+    }
+
+    function createOrder(address buyer, string calldata name, int price, int amount) public onlyShop{
+        Order memory _order;
+        _order.id = bytes16(keccak256(abi.encodePacked(msg.sender, blockhash(block.number-1))));
+        _order.name = name;
+        _order.price = price;
+        _order.amount = amount;
+        _order.buyer = buyer;
+        _order.state = State.PENDING;
+        _order.shop = shops[msg.sender];
+
+        // _ordersByShop[msg.sender].push(_order);
+        _ordersByBuyer[buyer].push(_order.id);
+        orders[_order.id] = _order;
+    }
+
+    
+    function cancelOrder(address buyer, bytes16 id) onlyShop buyerHasOrder(buyer, id) public returns (bool) {
+        require(orders[id].state != State.CONFIRMED, "Invalid State");
+        require(orders[id].state != State.REFUNDED, "Invalid State");
+        orders[id].state = State.CANCELED;
+        return true;
+    }
+
+    function confirmOrder(address buyer, bytes16 id) onlyAdmin buyerHasOrder(buyer, id) public returns (bool)  {
+        bytes32 _adminCountry = keccak256(abi.encodePacked(_admins[msg.sender].country));
+        bytes32 _orderCountry = keccak256(abi.encodePacked(orders[id].shop.country));
+        require(_adminCountry != _orderCountry, "Same country as the order created");
+        orders[id].state = State.CONFIRMED;
+        return true;
+    }
+
+    function rejectOrder(address buyer, bytes16 id) onlyAdmin buyerHasOrder(buyer, id) public returns (bool){
+        bytes32 _adminCountry = keccak256(abi.encodePacked(_admins[msg.sender].country));
+        bytes32 _orderCountry = keccak256(abi.encodePacked(orders[id].shop.country));
+        require(_adminCountry != _orderCountry, "Same country as the order created");
+        orders[id].state = State.REJECTED;
+        return true;
+    }
+
+    function getAllOrdersByBuyer(address buyer) public view returns (Order[] memory){
+        bytes16[] memory _id_list = _ordersByBuyer[buyer];
+        Order[] memory _order;
+        for (uint i = 0; i < _id_list.length;i++ ){
+            _order[i] = orders[_id_list[i]];
+        }
+        return _order;
+    }
+
+
+    function refund(bytes16[] orderIds) onlyAdmin public {
+        uint256 refundedAmount = 0;
+        for (uint i = 0; i < _orders.length; i++){
+            
+        }
+        
+    }
+
 }
