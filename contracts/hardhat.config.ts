9d9ff5adb8bb8ce657aad817ec73a87f9842e8dd
kengacethylene <keng2120@gmail.com>
2021-11-25 12:40:24 +0700

    delete unnecessary file

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -31,11 +31,6 @@ const config: HardhatUserConfig = {
   },
   solidity: "0.8.4",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
     hardhat: {
       chainId: 1337
     },
